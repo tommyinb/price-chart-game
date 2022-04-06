66025cbf9b22d9339009451750e5cf1e556492d1
Tommy <tommy@tom.my>
2022-04-06 10:35:52 +0800

    history

--- src/quizzes/Quiz.tsx
+++ src/quizzes/Quiz.tsx
@@ -1,4 +1,6 @@
 import { useCallback, useState } from "react";
+import { loadHistory } from "../histories/storages/loadHistory";
+import { saveHistory } from "../histories/storages/saveHistory";
 import { useSetting } from "../settings/useSetting";
 import { Answer } from "./Answer";
 import { Questions } from "./Questions";
@@ -10,9 +12,21 @@ export function Quiz() {
   const [answers, setAnswers] = useState<Answer[]>([]);
 
   const [completed, setCompleted] = useState(false);
+
   const complete = useCallback((answers: Answer[]) => {
     setAnswers(answers);
     setCompleted(true);
+
+    for (const answer of answers) {
+      if (answer.answered) {
+        const history = loadHistory(answer);
+
+        saveHistory({
+          ...history,
+          count: history.count + 1,
+        });
+      }
+    }
   }, []);
 
   return completed ? (
